9640ceec56fc2bd95a731179ed4685894f122fd5
Takuya Nemoto <takuslugger200@gmail.com>
2024-04-02 16:17:12 +0900

    feat: add import files

--- lib/importer.dart
+++ lib/importer.dart
@@ -1,3 +1,10 @@
 export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
 export 'dart:async';
+export 'package:gap/gap.dart';
+
+// custom files
+export "package:my_alarm/views/home.dart";
+export "package:my_alarm/constants/colors.dart";
+export 'package:my_alarm/components/alarm/alarm_card.dart';
+export 'package:my_alarm/components/alarm/alarm_config_screen.dart';
