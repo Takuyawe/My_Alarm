cbc1e2077f2d77db1aaaef74d1201425322a1404
Takuya Nemoto <takuslugger200@gmail.com>
2024-04-02 16:16:45 +0900

    feat: add colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:my_alarm/importer.dart';
+
+const baseDarkColor = Color(0xFF050023);
+const lightBlue = Color(0xFFC5CAE9);
+const grey = Color(0xFF616161);
+const white = Color(0xFFFFFFFF);
+const lightGrey = Color(0xFFBDBDBD);
