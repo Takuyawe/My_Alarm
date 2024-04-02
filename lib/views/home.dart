954caed02b95b9455a34c643ebc531bb2bb75376
Takuya Nemoto <takuslugger200@gmail.com>
2024-04-02 16:17:00 +0900

    feat: create home screen

---
+++ lib/views/home.dart
@@ -0,0 +1,26 @@
+import 'package:my_alarm/components/alarm/alarm_config_screen.dart';
+import 'package:my_alarm/importer.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: baseDarkColor,
+      appBar: AppBar(title: const Text("Alarm App")),
+      body: Container(
+          child: Row(
+        children: [
+          AlarmCard(),
+          AlarmCard(),
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: lightBlue,
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
