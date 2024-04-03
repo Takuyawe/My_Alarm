4be010a4c26240e3bbb3ceceeac85d0709459258
Takuya Nemoto <takuslugger200@gmail.com>
2024-04-03 13:04:23 +0900

    feat: add provider

---
+++ lib/providers/alarm_data_provider.dart
@@ -0,0 +1,22 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:my_alarm/importer.dart";
+
+final alarmRepositoryProvider = Provider<AlarmRepository>((ref) {
+  return AlarmRepository();
+});
+
+final sharedPreferencesInstanceProvider =
+    FutureProvider<SharedPreferences>((ref) async {
+  return await SharedPreferences.getInstance();
+});
+
+final alarmDataProvider = FutureProvider<Map<String, dynamic>>((ref) async {
+  final SharedPreferences prefs =
+      await ref.watch(sharedPreferencesInstanceProvider.future);
+
+  final String? alarmDataJson = prefs.getString("alarmData");
+
+  if (alarmDataJson == null) return {};
+
+  return jsonDecode(alarmDataJson);
+});
