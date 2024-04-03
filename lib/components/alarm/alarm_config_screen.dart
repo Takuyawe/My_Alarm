56394fd75ca08425a0f836e0c91f9cf5e4505a9e
Takuya Nemoto <takuslugger200@gmail.com>
2024-04-03 11:06:01 +0900

    refactor: remove unused code

--- lib/components/alarm/alarm_config_screen.dart
+++ lib/components/alarm/alarm_config_screen.dart
@@ -1,22 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-import 'package:flutter/material.dart';
 import 'package:my_alarm/importer.dart';
-
-// class AlarmConfigScreen extends StatelessWidget {
-//   const AlarmConfigScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//         child: Column(children: [
-//       Container(
-//         child: Text("10:00",
-//             style: TextStyle(
-//                 fontSize: 50, color: white, fontWeight: FontWeight.w500)),
-//       )
-//     ]));
-//   }
-// }
 
 void showAlarmConfigScreen(BuildContext context) {
   // TODO: receive alarm data here, if not set, add a new alarm
