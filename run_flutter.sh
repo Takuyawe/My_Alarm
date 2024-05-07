93d191bcd083da98022835542c6d4812b74619b3
Takuya <takuslugger200@gmail.com>
2024-05-07 19:09:31 +0900

    edit shell script

--- run_flutter.sh
+++ run_flutter.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# 現在実行中の Flutter macOS アプリのプロセスを探して終了させる
-pkill -f "flutter_tools.*macos"
+# Flutterアプリを起動
+flutter run -d macos &
 
-# Flutter アプリを再起動
-cd /Users/nemototakuya/workspace/dev/personal/my_alarm
-flutter run -d macos
+# FlutterプロセスのIDを取得
+FLUTTER_PID=$!
 
-# crontab -e
-# * */1 * * * /Users/nemototakuya/workspace/dev/personal/my_alarm/run_flutter.sh
+# Flutterプロセスが動作している間、30分ごとに "shift+r" を送信
+while kill -0 $FLUTTER_PID 2> /dev/null; do
+  # 1800秒待機 (30分)
+  sleep 1800
+  # "shift+r" を送信
+  cliclick kd:shift t:r ku:shift
+done
+
+echo "Flutter process has terminated."
